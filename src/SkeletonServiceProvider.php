5a5e5310e2cb0c70b1d02917865c664dcebfd7e1
Ryan Chandler <ryangjchandler@gmail.com>
2022-01-04 12:10:17 +0000

    chore: use PluginServiceProvider

--- src/SkeletonServiceProvider.php
+++ src/SkeletonServiceProvider.php
@@ -2,24 +2,10 @@
 
 namespace VendorName\Skeleton;
 
-use Spatie\LaravelPackageTools\Package;
-use Spatie\LaravelPackageTools\PackageServiceProvider;
 use VendorName\Skeleton\Commands\SkeletonCommand;
+use Filament\PluginServiceProvider;
 
-class SkeletonServiceProvider extends PackageServiceProvider
+class SkeletonServiceProvider extends PluginServiceProvider
 {
-    public function configurePackage(Package $package): void
-    {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
-        $package
-            ->name('skeleton')
-            ->hasConfigFile()
-            ->hasViews()
-            ->hasMigration('create_skeleton_table')
-            ->hasCommand(SkeletonCommand::class);
-    }
+    public static string $name = 'skeleton';
 }

c9e8298fcb503b1aa90b2221b0911632100885a7
Zep Fietje <hey@zepfietje.com>
2022-10-16 11:38:07 +0200

    Update Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,20 +2,20 @@ const colors = require("tailwindcss/colors");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./resources/views/**/*.blade.php", "./src/**/*.php"],
-  darkMode: "class",
-  theme: {
-    extend: {
-      colors: {
-          danger: colors.rose,
-          primary: colors.yellow,
-          success: colors.green,
-          warning: colors.amber,
-      },
+    content: ["./resources/views/**/*.blade.php", "./src/**/*.php"],
+    darkMode: "class",
+    theme: {
+        extend: {
+            colors: {
+                danger: colors.rose,
+                primary: colors.amber,
+                success: colors.green,
+                warning: colors.amber,
+            },
+        },
     },
-  },
-  corePlugins: {
-    preflight: false,
-  },
-  plugins: [],
+    corePlugins: {
+        preflight: false,
+    },
+    plugins: [],
 };
